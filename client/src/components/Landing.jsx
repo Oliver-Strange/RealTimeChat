c6fb469a2776160d627097447d7e9e0c8b5fd405
Oliver-Strange <preston.burton@gmail.com>
2020-01-24 23:32:16 -0800

    landing page styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div>
-      <h1>Welcome to Eugene Chat</h1>
-      <p>A new way to keep in touch with the city</p>
-      <Link to="/login">
-        <button>Sign in</button>
+    <div className="landingContainer">
+      <h1 className="title">Welcome to Eugene Chat</h1>
+      <p className="tagline">Connecting Citizens to the City</p>
+      <Link to="/login" className="loginLink">
+        <button className="signInButton">Sign in</button>
       </Link>
-      <Link to="/register">
-        <button>Register</button>
+      <Link to="/register" className="registerLink">
+        <button className="regButton">Register</button>
       </Link>
     </div>
   );
